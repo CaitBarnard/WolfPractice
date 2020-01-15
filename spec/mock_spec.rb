699682ef5d7aa12bc7b2788049564505fc19f70e
cait <caitlin@madetech.com>
2020-01-15 09:12:42 +0000

    Added test for mock

--- spec/mock_spec.rb
+++ spec/mock_spec.rb
@@ -1,9 +1,33 @@
 require 'net/http'
 require 'weather'
 
+class MockClient
+
+    attr_accessor :get_is_called
+
+    def initialize
+        @get_is_called = false
+    end
+
+    def get(url)
+        @get_is_called = true
+    end
+
+    def verifyGet(test)
+        test.expect(get_is_called).to test.eq(true)
+    end
+end
+
 describe 'Mock' do
 
-    it "" do
+    it "verifies that it got something" do
+        client = MockClient.new
+
+        weatherAPI = Weather.new(client)
+        weatherAPI.location = 'barcelona'
+
+        response = weatherAPI.sendRequest
+        client.verifyGet(self)
     end
 
 end
